6517527f2fa33525587aa722db52eb2d72d0889b
Kaan Şengün <427642@ogr.ktu.edu.tr>
2023-11-05 21:07:24 +0300

    Initial Commit

---
+++ OverridingExample/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OverridingExample
+//
+//  Created by Kaan Şengün on 5.11.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
