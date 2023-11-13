d1aff5aa797e8ffbe7358e1bc29f5970de133230
Kaan Şengün <427642@ogr.ktu.edu.tr>
2023-11-13 09:36:05 +0300

    Commit oldu

--- OverridingExample/main.cpp
+++ OverridingExample/main.cpp
@@ -6,9 +6,35 @@
 //
 
 #include <iostream>
+using namespace std;
+
+class Hayvan{
+public:
+    virtual string speak(){
+        return "Hayvanlar";
+    }
+};
+class Kedi:public Hayvan{
+public:
+    string speak(){
+        return "Miyav";
+    }
+};
+class Kopek:public Hayvan{
+public:
+    string speak(){
+        return "Hav";
+    }
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    Hayvan *Hayvan=new Kopek();
+    //Hayvan *Hayvan=new Kedi();
+    cout<<"Ne sesi geldi: "<<Hayvan->speak()<<endl;
+    delete Hayvan;
+    
+    
+    
     return 0;
 }
